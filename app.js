d11e6e1140e3e644a9fb8f2e9a9b8e5fcfaefddf
srayas <srayaskv@gmail.com>
2021-01-20 02:05:29 +0530

    l3 security by enviroinment variable by dotenv and gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption"); 
 
 const app = express();
+
+const secret=process.env.SECRET;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -20,7 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
+
 
 userSchema.plugin(encrypt,{ secret: secret, encryptedFields: ["password"] });
 
